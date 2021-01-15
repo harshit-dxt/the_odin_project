d023f4c91ba1912d846e231ee60349324af07755
Harshit Dixit <harshitdxt3004@gmail.com>
2021-01-15 19:31:43 +0530

    Etch-a-sketch: Added color picker functionality

--- etch-a-sketch/script/index.js
+++ etch-a-sketch/script/index.js
@@ -1,3 +1,6 @@
+let rainbowIndex = 0;
+const allowedColor = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
+
 function createGrid(){
     const gridContainer = document.querySelector('.grid-container');
     const gridSize = 50;
@@ -10,50 +13,47 @@ function createGrid(){
     }
 }
 
-function resetGrid(){
+function setGridColor(color, reset){
     gridItemAll.forEach((gridItem) => {
-        gridItem.style.backgroundColor = 'white';
-        
+        if(reset){
+            gridItem.style.backgroundColor='white';
+        }
         gridItem.addEventListener('mouseover', (e)=>{
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = color;
         })
     })
+}
+
+function resetGrid(){
+    setGridColor('black', true);
 }
 
 createGrid();
 
 const gridItemAll = document.querySelectorAll('.grid-item');
-let color = 0;
-const allowedColor = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
+
 resetGrid();
 
 function eraserActivate(){
-    gridItemAll.forEach((gridItem) => {
-        gridItem.addEventListener('mouseover', (e)=>{
-            e.target.style.backgroundColor = 'white';
-        })
-    })
+    setGridColor('white', false);
 }
 
 function rainbowActivate(){
     gridItemAll.forEach((gridItem)=>{
         gridItem.addEventListener('mouseover', (e)=>{
-            console.log(e.target.classList);
-            e.target.style.backgroundColor = allowedColor[color];
-            color = (color+1) % 7;
+            e.target.style.backgroundColor = allowedColor[rainbowIndex];
+            rainbowIndex = (rainbowIndex+1) % 7;
         })
     })
 }
 
 function eraserDeactivate(){
-    gridItemAll.forEach((gridItem) => {
-        gridItem.addEventListener('mouseover', (e)=>{
-            e.target.style.backgroundColor = 'black';
-        })
-    })
+    setGridColor('black', false);
 }
 
-
+function changeColor(e){
+    setGridColor(e.target.value, false);
+}
 
 const eraserBtn = document.querySelector('#eraser');
 eraserBtn.addEventListener('click', eraserActivate);
@@ -66,3 +66,6 @@ blackBtn.addEventListener('click', eraserDeactivate);
 
 const rainbowBtn = document.querySelector('#rainbow');
 rainbowBtn.addEventListener('click', rainbowActivate);
+
+const picker = document.querySelector('#picker');
+picker.addEventListener('change', changeColor);
